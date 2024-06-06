b7f420fd8a6c11ba1f30b73ba8a5d1d79cabc5f4
waltertaya <waltermittybecker5@gmail.com>
2024-06-06 22:12:20 +0300

    Updated: lockboxes - requirement fail, pep 8

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,9 +2,10 @@
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
-    
+
     Parameters:
-    boxes (list of list of int): A list of boxes, each containing a list of keys.
+    boxes (list of list of int): A list of
+    boxes, each containing a list of keys.
 
     Returns:
     bool: True if all boxes can be opened, else False.
@@ -13,14 +14,13 @@ def canUnlockAll(boxes):
     opened = [False] * n
     opened[0] = True
     stack = [0]
-    
+
     while stack:
         current_box = stack.pop()
-        
+
         for key in boxes[current_box]:
             if key < n and not opened[key]:
                 opened[key] = True
                 stack.append(key)
-    
-    return all(opened)
 
+    return all(opened)
